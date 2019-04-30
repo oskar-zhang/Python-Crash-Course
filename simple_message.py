11ba5db8d1aebed6e55bc56fa809b76fefc9364e
oskarsforest <oskarzhang1203@gmail.com>
2019-04-29 22:56:57 -0700

    Updated simple message again

--- simple_message.py
+++ simple_message.py
@@ -1,3 +1,7 @@
 message = "Store a message in a variable, and then print that message."
 
 print(message)
+
+message = "Store a message in a variable, and print that message. Then change the value of your variable to a new message, and print the new message."
+
+print(message)
