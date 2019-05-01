d7b8be922eeafa5edc7303c6f49b4f5b6558a4d8
oskarsforest <oskarzhang1203@gmail.com>
2019-04-30 21:38:25 -0700

    Changing Case in a String with Methods

--- name.py
+++ name.py
@@ -3,22 +3,3 @@ print(name.title())
 
 print(name.upper())
 print(name.lower())
-
-first_name = "ada"
-last_name = "lovelace"
-full_name = first_name + " " + last_name
-
-print(full_name)
-
-print("Hello, " + full_name + "!")
-
-message = "Hello, " + full_name.title() + "!"
-
-print(message)
-
-print("\t" + message)
-
-print("Languages:\nPython\nC++\nJavascript")
-
-print("Languages:\nPython\n\tC++\nJavascript")
-
