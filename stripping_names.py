69853b4c113dd40cdc0faf6a7289671002d7b96d
oskarsforest <oskarzhang1203@gmail.com>
2019-05-03 22:13:03 -0700

    Try It Yourself

---
+++ stripping_names.py
@@ -0,0 +1,23 @@
+"""
+2-7. Stripping Names: Store a person’s name, and include some whitespace
+characters at the beginning and end of the name. Make sure you use each
+character combination, "\t" and "\n", at least once.
+Print the name once, so the whitespace around the name is displayed.
+Then print the name using each of the three stripping functions, lstrip(),
+rstrip(), and strip().
+
+"""
+
+name = " Oskar Zhang "
+
+print("\t Oskar Zhang")
+
+print("\t" + name)
+
+print("Oskar\nZhang")
+
+print(name.lstrip())
+
+print(name.rstrip())
+
+print(name.strip())
