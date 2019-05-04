a186a8410fdcf19a173b5f3d4aee0b4aae89bd93
oskarsforest <oskarzhang1203@gmail.com>
2019-05-04 08:18:15 -0700

    Try It Yourself

---
+++ favorite_number.py
@@ -0,0 +1,11 @@
+"""
+2-9. Favorite Number: Store your favorite number in a variable. Then, using
+that variable, create a message that reveals your favorite number. Print that
+message.
+"""
+
+fav_num = 12
+
+message = "My Favorite number is " + str(fav_num) +"."
+
+print(message)
