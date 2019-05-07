0045f4d4eeaa9a011a3656caf79c350c59049ab3
oskarsforest <oskarzhang1203@gmail.com>
2019-05-06 23:05:52 -0700

    Try It Yourself

---
+++ Chapter 3 Introducing Lists/names.py
@@ -0,0 +1,12 @@
+"""
+3-1. Names: Store the names of a few of your friends in a list called names. Print
+each person’s name by accessing each element in the list, one at a time.
+
+"""
+
+friends = ['Greg', 'Sun', 'Elton','A Dai']
+
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
