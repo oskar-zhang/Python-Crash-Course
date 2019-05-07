45042c2e1b2c06d1320c3ccfff1ae6fa6c39be2e
oskarsforest <oskarzhang1203@gmail.com>
2019-05-06 23:02:52 -0700

    Moved to a new folder

---
+++ Chapter 3 Introducing Lists/bicycles.py
@@ -0,0 +1,12 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+print(bicycles[0])
+
+print(bicycles[1].title())
+
+print(bicycles[-1])
+
+message = "My first bicycle was a " + bicycles[-1].title() + "."
+
+print(message)
