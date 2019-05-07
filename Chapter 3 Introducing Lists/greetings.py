fd04f9c597bc85af8b1e8f1ec21e77fb25759aa8
oskarsforest <oskarzhang1203@gmail.com>
2019-05-06 23:08:50 -0700

    Try It Yourself

---
+++ Chapter 3 Introducing Lists/greetings.py
@@ -0,0 +1,21 @@
+"""
+3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just
+printing each person’s name, print a message to them. The text of each message
+should be the same, but each message should be personalized with the
+person’s name.
+"""
+
+friends = ['Greg', 'Sun', 'Elton','A Dai']
+
+message1 = "You are my friend, " + friends[0] + "."
+
+message2 = "You are my friend, " + friends[1] + "."
+
+message3 = "You are my friend, " + friends[2] + "."
+
+message4 = "You are my friend, " + friends[3] + "."
+
+print(message1)
+print(message2)
+print(message3)
+print(message4)
