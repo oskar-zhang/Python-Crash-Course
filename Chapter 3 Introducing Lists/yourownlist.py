0c5976ac336ceba12667698e1a511a62b3699031
oskarsforest <oskarzhang1203@gmail.com>
2019-05-06 23:12:19 -0700

    Try It Yourself

---
+++ Chapter 3 Introducing Lists/yourownlist.py
@@ -0,0 +1,12 @@
+"""
+3-3. Your Own List: Think of your favorite mode of transportation, such as a
+motorcycle or a car, and make a list that stores several examples. Use your list
+to print a series of statements about these items, such as “I would like to own a
+Honda motorcycle.”
+"""
+
+cars = ['Audi TT', 'Tesla Model S', 'Audi Q8']
+
+message = "I would like to own a " + cars[0] + "."
+
+print(message)
