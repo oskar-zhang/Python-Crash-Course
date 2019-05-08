19ff5618d1adca79296c6221ca25f515b98e35f8
oskarsforest <oskarzhang1203@gmail.com>
2019-05-07 22:40:16 -0700

    Removing Elements from a List

--- Chapter 3 Introducing Lists/motorcycles.py
+++ Chapter 3 Introducing Lists/motorcycles.py
@@ -21,3 +21,29 @@ print(motorcycles)
 
 motorcycles.insert(4,'Dadi')
 print(motorcycles)
+
+del motorcycles[0]
+print(motorcycles)
+
+poped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(poped_motorcycle)
+
+last_owned = motorcycles.pop()
+print("The last motorcycle I owned was a " + last_owned.title() + ".")
+
+first_owned = motorcycles.pop(0)
+print('The first motorcycle I owned was a ' + first_owned.title() + '.')
+
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+motorcycles.remove('ducati')
+print(motorcycles)
+
+too_expensive = 'yamaha'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
