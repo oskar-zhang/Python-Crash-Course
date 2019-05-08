54ca3b99cc4eb8da55fed4ed187e0e42d3034134
oskarsforest <oskarzhang1203@gmail.com>
2019-05-07 22:18:45 -0700

    Adding Elements to a List

--- Chapter 3 Introducing Lists/motorcycles.py
+++ Chapter 3 Introducing Lists/motorcycles.py
@@ -3,3 +3,21 @@ print(motorcycles)
 
 motorcycles[0] = 'ducati'
 print(motorcycles)
+
+motorcycles.append('Feige')
+print(motorcycles)
+
+motorcycles = []
+
+print(motorcycles)
+
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+motorcycles.insert(4,'Dadi')
+print(motorcycles)
