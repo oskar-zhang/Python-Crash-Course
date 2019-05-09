d5c9d3dad6515efda3e7a29ddf3d0f6efd3304b0
oskarsforest <oskarzhang1203@gmail.com>
2019-05-08 21:07:43 -0700

    Try It Yourself

---
+++ Chapter 3 Introducing Lists/guest_list.py
@@ -0,0 +1,14 @@
+"""
+3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who
+would you invite? Make a list that includes at least three people you’d like to
+invite to dinner. Then use your list to print a message to each person, inviting
+them to dinner.
+"""
+
+guest_list = ['Greg', 'Elton', 'Stephen']
+
+print("I would like to invite you to my party, " + guest_list[0] + ".")
+
+print("I would like to invite you to my party, " + guest_list[1] + ".")
+
+print("I would like to invite you to my party, " + guest_list[2] + ".")
