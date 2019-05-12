56d274c425537cac1f8a35f54c4865493fcaa3d0
oskarsforest <oskarzhang1203@gmail.com>
2019-05-11 22:40:45 -0700

    Sort a List Temporarily with the sorted() Function

--- Chapter 3 Introducing Lists/cars.py
+++ Chapter 3 Introducing Lists/cars.py
@@ -4,3 +4,17 @@ print(cars)
 
 cars.sort(reverse=True)
 print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the reversed sorted list:")
+print(sorted(cars, reverse=True))
+
+print("\nHere is the origianl list:")
+print(cars)
