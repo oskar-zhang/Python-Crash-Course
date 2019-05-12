f858421882317a47e7dc8cfb9404637ed12c6eee
oskarsforest <oskarzhang1203@gmail.com>
2019-05-11 23:03:09 -0700

    Try It Yourself

---
+++ Chapter 3 Introducing Lists/dinner_guest.py
@@ -0,0 +1,9 @@
+"""
+3-9. Dinner Guests: Working with one of the programs from Exercises 3-4
+through 3-7 (page 46), use len() to print a message indicating the number
+of people you are inviting to dinner.
+"""
+
+guest_list = ['Greg', 'Elton', 'Stephen']
+
+print("\nThere are " + str(len(guest_list)) + " guests.")
