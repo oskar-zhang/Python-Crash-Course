b072155310588c46ab1341c81b74884ab097bc39
oskarsforest <oskarzhang1203@gmail.com>
2019-05-15 20:22:42 -0700

    Try It Yourself

---
+++ Chapter 4 Working With Lists/threes.py
@@ -0,0 +1,8 @@
+"""
+4-7. Threes: Make a list of the multiples of 3 from 3 to 30. Use a for loop to
+print the numbers in your list.
+"""
+
+threes = [number*3 for number in range(3,31)]
+
+print(threes)
