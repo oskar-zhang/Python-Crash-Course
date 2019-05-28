4ccf9a1e1947ec3896056c7a019ea7aa606ee0da
oskarsforest <oskarzhang1203@gmail.com>
2019-05-27 18:08:20 -0700

    Styling Your Code

---
+++ Chapter 4 Working With Lists/styling.py
@@ -0,0 +1,21 @@
+#PEP 8 recommends that you use four spaces per indentation level
+
+"""
+You should definitely use your tab key,
+but also make sure your editor is set to insert spaces rather than tabs into
+your document.
+"""
+
+#Many Python programmers recommend that each line should be less than
+#80 characters.
+
+"""
+PEP 8 also recommends that you limit
+all of your comments to 72 characters per line, because some of the tools
+that generate automatic documentation for larger projects add formatting
+characters at the beginning of each commented line.
+"""
+
+#To group parts of your program visually, use blank lines. You should use
+#blank lines to organize your files, but don’t do so excessively.
+
