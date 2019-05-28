8ebe50e973d62ab6804b1ac1d57359f4e7029a6b
oskarsforest <oskarzhang1203@gmail.com>
2019-05-27 18:24:05 -0700

    Checking Multiple Conditions

--- Chapter 5 If Statements/magix_number.py
+++ Chapter 5 If Statements/magix_number.py
@@ -2,3 +2,32 @@ answer = 12
 
 if answer != 3:
     print("you have the worng answer. Please try again!")
+
+
+#Using and to Check Multiple Conditions
+
+"""
+To improve readability, you can use parentheses around the individual
+tests, but they are not required.
+
+(age_0 >= 21) and (age_1 >= 21)
+
+"""
+
+#Checking Whether a Value Is in a List
+
+"""
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+'mushrooms' in requested_toppings
+True
+
+'pepperoni' in requested_toppings
+False
+"""
+
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+abc = 'mushrooms' in requested_toppings
+print(abc)
+
+bcd = 'pepperoni' in requested_toppings
+print(bcd)
