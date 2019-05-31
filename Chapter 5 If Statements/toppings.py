55966ccfce251097fc26eb838cff8ce793e01543
oskarsforest <oskarzhang1203@gmail.com>
2019-05-30 23:15:11 -0700

    Testing Multiple Conditions

--- Chapter 5 If Statements/toppings.py
+++ Chapter 5 If Statements/toppings.py
@@ -2,3 +2,25 @@ requsted_topping = 'mushrooms'
 
 if requsted_topping != 'blueberries':
     print("You have the wrong toppings!")
+
+
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
+
+
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+elif 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+elif 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
