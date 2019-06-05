764b5f002de3ba596e65f0d3946e344ef71ab39a
oskarsforest <oskarzhang1203@gmail.com>
2019-06-04 22:59:49 -0700

    Modifying Values in a Dictionary

--- Chapter 6 Dictionaries/alien.py
+++ Chapter 6 Dictionaries/alien.py
@@ -28,3 +28,20 @@ print("The alien is " + alien_0['color'] + ".")
 
 alien_0['color'] = 'yellow'
 print("The alien is now " + alien_0['color'] + ".")
+
+#More interesting example
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print("Original x-position: " + str(alien_0['x_position']))
+
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print("New x-position: " + str(alien_0['x_position']))
