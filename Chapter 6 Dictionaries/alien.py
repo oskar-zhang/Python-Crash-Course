a1d3db56102792a5933dc3e853ad5a3d0f1487c7
oskarsforest <oskarzhang1203@gmail.com>
2019-06-04 22:45:42 -0700

    Adding New Key-Value Pairs

--- Chapter 6 Dictionaries/alien.py
+++ Chapter 6 Dictionaries/alien.py
@@ -6,3 +6,9 @@ print(alien_0['points'])
 new_points = alien_0['points']
 
 print("You just earned " + str(new_points) +" points!")
+
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+
+print(alien_0)
