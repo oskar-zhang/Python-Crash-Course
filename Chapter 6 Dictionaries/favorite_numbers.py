b7da00a67af7f72c50717b3aa52709d5338246c4
oskarsforest <oskarzhang1203@gmail.com>
2019-06-07 22:45:52 -0700

    Try It Yourself

---
+++ Chapter 6 Dictionaries/favorite_numbers.py
@@ -0,0 +1,19 @@
+"""
+6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers.
+Think of five names, and use them as keys in your dictionary. Think of a favorite
+number for each person, and store each as a value in your dictionary. Print
+each person’s name and their favorite number. For even more fun, poll a few
+friends and get some actual data for your program.
+"""
+
+fav_nums ={'Greg': 13, 'Lane': 10, 'Elton': 7, 'Oskar': 3, 'Port': 4}
+
+print("Greg's favorite number is " + str(fav_nums['Greg']))
+
+print("Lane's favorite number is " + str(fav_nums['Lane']))
+
+print("Elton's favorite number is " + str(fav_nums['Elton']))
+
+print("Oskar's favorite number is " + str(fav_nums['Oskar']))
+
+print("Port's favorite number is " + str(fav_nums['Port']))
