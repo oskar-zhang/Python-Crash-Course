97c61453a362d723e44d8be576473040cc365945
oskarsforest <oskarzhang1203@gmail.com>
2019-06-08 23:26:32 -0700

    Looping Through All the Keys

--- Chapter 6 Dictionaries/favorite_languages.py
+++ Chapter 6 Dictionaries/favorite_languages.py
@@ -8,5 +8,11 @@ favorite_languages = {
 print("Sarah's favorite language is " + favorite_languages['sarah'].title() + 
 ".")
 
+#Looping Through All Key-Value Pairs
 for name, language in favorite_languages.items():
     print("\n" + name.title() + "'s favorite language is " + language.title())
+
+
+#Looping Through All the Keys in a Dictionary
+for name in favorite_languages.keys():
+    print(name.title())
