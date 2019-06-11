067501ab3d6b6682ca470a96d8ba4a9453138340
oskarsforest <oskarzhang1203@gmail.com>
2019-06-10 23:21:29 -0700

    Try It Yourself

---
+++ Chapter 6 Dictionaries/rivers.py
@@ -0,0 +1,23 @@
+"""
+6-5. Rivers: Make a dictionary containing three major rivers and the country
+each river runs through. One key-value pair might be 'nile': 'egypt'.
+• Use a loop to print a sentence about each river, such as The Nile runs
+through Egypt.
+• Use a loop to print the name of each river included in the dictionary.
+• Use a loop to print the name of each country included in the dictionary.
+"""
+
+rivers = {
+    'yellow river': 'china',
+    'nile': 'egypt',
+    'Mississippi River': 'united states'
+}
+
+for river, country in rivers.items():
+    print("\nThe " + river.title() + " runs through " + country.title())
+
+for river in rivers.keys():
+    print(river.title())
+
+for country in rivers.values():
+    print(country.title())
