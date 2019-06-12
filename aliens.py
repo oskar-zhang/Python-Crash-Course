a9c322fbaaf0e56294bd0b7a13fda08895274933
oskarsforest <oskarzhang1203@gmail.com>
2019-06-11 21:24:34 -0700

    Stronger alien

--- aliens.py
+++ aliens.py
@@ -17,6 +17,12 @@ for alien_number in range(30):
     new_alien = {'color': 'green', 'point': 5, 'speed': 'slow'}
     aliens.append(new_alien)
 
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['point'] = 10
+        alien['speed'] = 'medium'
+
 #Show the first 5 aliens
 
 for alien in aliens[:5]:
