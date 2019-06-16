56460ef2a60e03012c7b87511b4c2dad801a6ec5
oskarsforest <oskarzhang1203@gmail.com>
2019-06-15 23:25:29 -0700

    Summary

---
+++ Chapter 6 Dictionaries/summary.py
@@ -0,0 +1,19 @@
+"""
+In this chapter I learned how to define a dictionary and how to work
+with the information stored in a dictionary.
+"""
+
+"""
+I learned how to access and modify individual elements in a dictionary, 
+and how to loop through all of the information in a dictionary.
+"""
+
+"""
+I learned to loop through a dictionary’s key-value pairs, its keys, 
+and its values.
+"""
+
+"""
+I also learned how to nest multiple dictionaries in a list, nest lists in a 
+dictionary, and nest a dictionary inside a dictionary.
+"""
