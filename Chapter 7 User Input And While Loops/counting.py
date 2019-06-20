de4b0625c322c2979520aefcf1583895b716bbdc
oskarsforest <oskarzhang1203@gmail.com>
2019-06-19 23:02:43 -0700

    Avoiding Infinite Loops

--- Chapter 7 User Input And While Loops/counting.py
+++ Chapter 7 User Input And While Loops/counting.py
@@ -16,3 +16,8 @@ while current_number < 10:
 #If the modulo is 0 (which means current_number is divisible by 2), 
 #the continue statement tells Python to ignore the rest of the loop 
 # and return to the beginning.
+
+# This loop runs forever!
+x = 1
+while x <= 5:
+    print(x)
