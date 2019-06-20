5adeffc45ba29212d366a2f6c649fe1b351e2b36
oskarsforest <oskarzhang1203@gmail.com>
2019-06-19 23:01:10 -0700

    Using continue in a Loop

--- Chapter 7 User Input And While Loops/counting.py
+++ Chapter 7 User Input And While Loops/counting.py
@@ -1,4 +1,18 @@
+"""
 current_number = 1
 while current_number <= 5:
     print(current_number)
     current_number +=1
+"""
+
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
+    
+    print(current_number)
+
+#If the modulo is 0 (which means current_number is divisible by 2), 
+#the continue statement tells Python to ignore the rest of the loop 
+# and return to the beginning.
