d3d021c5651ea1a009f4362f97e6fc5949b3e2ff
oskarsforest <oskarzhang1203@gmail.com>
2019-06-20 22:37:50 -0700

    First

---
+++ Chapter 7 User Input And While Loops/three_exits.py
@@ -0,0 +1,42 @@
+"""
+7-6. Three Exits: Write different versions of either Exercise 7-4 or Exercise 7-5
+that do each of the following at least once:
+• Use a conditional test in the while statement to stop the loop.
+• Use an active variable to control how long the loop runs.
+• Use a break statement to exit the loop when the user enters a 'quit' value.
+"""
+
+#Use a conditional test in the while statement to stop the loop.
+message = input("How old are you? ")
+
+while message != 'quit':
+    age = int(message)
+    if age < 3:
+        print("FREE!")
+        message = input("How old are you? (Please type 'quit' if you are done.)")
+    elif age <= 12:
+        print("Your ticket cost $10.")
+        message = input("How old are you? (Please type 'quit' if you are done.)")
+    else:
+        print("You need to pay $15 for this ticket.")
+        message = input("How old are you? (Please type 'quit' if you are done.)")
+#Use an active variable to control how long the loop runs.
+"""
+question = "How old are you? "
+
+active = True
+
+while active:
+    age = int(input(question))
+
+    if age < 3:
+        print("FREE!")
+        active = False
+    elif age <= 12:
+        print("Your ticket cost $10.")
+        active = False
+    else:
+        print("You need to pay $15 for this ticket.")
+        active = False
+"""
+#Use a break statement to exit the loop when the user enters a 'quit' value.
