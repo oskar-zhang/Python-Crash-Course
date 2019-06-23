d2b03e974da5d23bfd4e56fa1b05a58a71f51b33
oskarsforest <oskarzhang1203@gmail.com>
2019-06-22 23:44:43 -0700

    Try It Yourself

---
+++ Chapter 7 User Input And While Loops/dream_vacation.py
@@ -0,0 +1,24 @@
+"""
+7-10. Dream Vacation: Write a program that polls users about their dream
+vacation. Write a prompt similar to If you could visit one place in the world,
+where would you go? Include a block of code that prints the results of the poll.
+"""
+
+dream_vacation = {}
+
+polling_active = True
+
+while polling_active:
+    name = input("\nWhat's your name? ")
+    response = input("\nIf you could visit one place in the world, where would you go?")
+    
+    dream_vacation[name] = response
+
+    repeat = input("Would you like to let another person respond? (yes/ no) ")
+    if repeat == 'no':
+        polling_active = False
+
+print("\n--- Poll Results ---")
+
+for name, response in dream_vacation.items():
+    print(name + " wants to go " + response +".")
