db484517d88e3a12e5029c4540180a51e98c8024
oskarsforest <oskarzhang1203@gmail.com>
2019-06-23 21:17:41 -0700

    Summary

---
+++ Chapter 7 User Input And While Loops/summary.py
@@ -0,0 +1,13 @@
+# I learned how to use input() to allow users to provide their own information 
+# in the programs
+
+# I learned to work with both text and numerical input and how to use 
+# while loops to make my programs run as long as I want them to
+
+# I learned several ways to control the flow of a while loop by setting 
+# an active flag, using the break statement, and using the continue statement.
+
+# I learned how to use a while loop to move items from one list to another 
+# and how to remove all instances of a value from a list.
+
+# I also learned how while loops can be used with dictionaries.
