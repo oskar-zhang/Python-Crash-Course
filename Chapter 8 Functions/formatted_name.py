da9166d3f2cb9edf0fd4453e1af9f67331b6a020
oskarsforest <oskarzhang1203@gmail.com>
2019-06-26 21:45:03 -0700

    Making an Argument Optional

--- Chapter 8 Functions/formatted_name.py
+++ Chapter 8 Functions/formatted_name.py
@@ -1,7 +1,29 @@
+"""
 def get_formatted_name(first_name, last_name):
-    """Return a full name, neatly formatted."""
+    #Return a full name, neatly formatted.
     full_name = first_name + ' ' + last_name
     return full_name.title()
 
 musician = get_formatted_name('jimi', 'hendrix')
 print(musician)
+"""
+
+#Making an Argument Optional
+
+def get_formatted_name(first_name, last_name, middle_name=''):
+    if middle_name:
+        full_name = first_name + ' ' + middle_name + ' ' + last_name
+    else:
+        full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+#If we’re using a middle name, however, we have to make sure the middle
+#name is the last argument passed so Python will match up the positional
+#arguments correctly
+
+musician = get_formatted_name('john', 'hooker', 'lee')
+print(musician)
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+
