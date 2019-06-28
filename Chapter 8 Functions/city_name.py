5887449792cf53923476f0d5854b94e3aefd3110
oskarsforest <oskarzhang1203@gmail.com>
2019-06-27 22:13:51 -0700

    Try It Yourself

---
+++ Chapter 8 Functions/city_name.py
@@ -0,0 +1,20 @@
+"""
+8-6. City Names: Write a function called city_country() that takes in the name
+of a city and its country. The function should return a string formatted like this:
+"Santiago, Chile"
+Call your function with at least three city-country pairs, and print the value
+that’s returned.
+"""
+
+def city_country(city, country):
+    message = city + ', ' + country
+    return message
+
+city_and_country = city_country('Beijing', 'China')
+print(city_and_country)
+
+city_and_country = city_country('Washington DC', 'USA')
+print(city_and_country)
+
+city_and_country = city_country('Tokyo', 'Japan')
+print(city_and_country)
