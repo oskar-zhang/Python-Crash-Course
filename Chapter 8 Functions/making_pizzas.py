09378cf7f2ad2698ac9613608f4bd83e04bfa660
oskarsforest <oskarzhang1203@gmail.com>
2019-07-09 22:25:17 -0700

    Using as to Give a Function an Alias

--- Chapter 8 Functions/making_pizzas.py
+++ Chapter 8 Functions/making_pizzas.py
@@ -7,8 +7,16 @@ pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
 """
 
 #Importing Specific Functions
-
+"""
 from pizza import make_pizza1
 
 make_pizza1('pepperoni')
 make_pizza1('mushrooms', 'green peppers', 'extra cheese')
+"""
+
+#Using as to Give a Function an Alias
+
+from pizza import make_pizza as mp
+
+mp(16, 'pepperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
