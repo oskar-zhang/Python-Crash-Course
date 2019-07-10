2fa5632ea9ceb126d891625abce253ac340a2f8d
oskarsforest <oskarzhang1203@gmail.com>
2019-07-09 22:22:24 -0700

    Importing Specific Functions

--- Chapter 8 Functions/making_pizzas.py
+++ Chapter 8 Functions/making_pizzas.py
@@ -1,4 +1,14 @@
+#Importing an Entire Module
+"""
 import pizza
 
 pizza.make_pizza(16, 'pepperoni')
 pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+"""
+
+#Importing Specific Functions
+
+from pizza import make_pizza1
+
+make_pizza1('pepperoni')
+make_pizza1('mushrooms', 'green peppers', 'extra cheese')
