85ca7ccfe5e9b64a38499b945b07ac61c90aed87
oskarsforest <oskarzhang1203@gmail.com>
2019-07-16 22:53:55 -0700

    summary

---
+++ Chapter 8 Functions/summary.py
@@ -0,0 +1,25 @@
+# In this chapter
+
+"""
+I learned how to write functions and to pass arguments
+so that your functions have access to the information they need to do their
+work.
+"""
+
+# I learned how to use positional and keyword arguments, and how to accept 
+# an arbitrary number of arguments.
+
+"""
+I saw functions that display output and functions that return values.
+"""
+
+# I learned how to use functions with lists, dictionaries, 
+# if statements, and while loops.
+
+"""
+I also saw how to store my functions in separate files called modules, 
+so my program files will be simpler and easier to understand.
+"""
+
+# I learned to style my functions so my programs will continue to be 
+# well-structured and as easy as possible for me and others to read.
