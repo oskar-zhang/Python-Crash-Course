e4e636acb668bfb35449d8ba7f4cf25f1d728475
oskarsforest <oskarzhang1203@gmail.com>
2019-07-25 23:09:53 -0700

    Try It Yourself

--- Chapter 8 Functions/album.py
+++ Chapter 8 Functions/album.py
@@ -11,6 +11,42 @@ of tracks, add that value to the album’s dictionary. Make at least one new
 function call that includes the number of tracks on an album.
 """
 
+def make_album(artist_name, album_title, tracks=''):
+
+    album = {'Artist Name': artist_name.title(), 'Title': album_title.title()}
+
+    if tracks:
+        album['Tracks'] = tracks
+
+    return album
+
+
+album = make_album('Michael Jackson', 'Thriller', 9)
+print(album)
+
+album = make_album('eagles', 'their greatest hits (1971–1975)')
+print(album)
+
+album = make_album('Pink Floyd', 'The Dark Side of the Moon')
+print(album)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 def make_album(name, title, number=''):
     album = {'Artist Name': name, 'Album Title': title}
     if number:
@@ -19,3 +55,4 @@ def make_album(name, title, number=''):
 
 new_album = make_album('Taylor Swift', 'You Need To Calm Down', number=1)
 print(new_album)
+"""
