0be77dd6094e39b294b46e55bb339f8025d44dd2
oskarsforest <oskarzhang1203@gmail.com>
2019-07-25 23:19:06 -0700

    Try It Yourself

--- Chapter 8 Functions/user_albums.py
+++ Chapter 8 Functions/user_albums.py
@@ -5,6 +5,53 @@ information, call make_album() with the user’s input and print the dictionary
 that’s created. Be sure to include a quit value in the while loop.
 """
 
+def make_album(artist_name, album_title, tracks=''):
+
+    album = {'Artist Name': artist_name.title(), 'Title': album_title.title()}
+
+    if tracks:
+        album['Tracks'] = tracks
+
+    return album
+
+while True:
+    print("\nPlease enter the album artist name and title:")
+    print("(enter 'q' at any time to quit)")
+
+    artist_name = input("Artist name: ")
+    if artist_name == 'q':
+        break
+
+    album_title = input("Album name: ")
+    if album_title == 'q':
+        break
+    
+    album = make_album(artist_name, album_title)
+    print(album)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 def make_album(name, title, number=''):
     album = {'Artist Name': name, 'Album Title': title}
     if number:
@@ -20,3 +67,5 @@ while True:
     check = input("\nEnter 'q' if you want to quit. ")
     if check == 'q':
         break
+
+"""
