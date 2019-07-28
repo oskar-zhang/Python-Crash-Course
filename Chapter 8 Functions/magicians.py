9c0a068a9b0e8a6a5a2346769244ab794581c4be
oskarsforest <oskarzhang1203@gmail.com>
2019-07-27 23:47:01 -0700

    Try It Yourself

--- Chapter 8 Functions/magicians.py
+++ Chapter 8 Functions/magicians.py
@@ -5,8 +5,39 @@ called show_magicians(), which prints the name of each magician in the list.
 
 magicians = ['David Copperfield', 'Criss Angel', 'Penn & Teller', 'Mat Franco']
 
+def show_magicians(names):
+        for name in names:
+                print(name)
+
+show_magicians(magicians)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 def show_magicians(magicians):
     for magician in magicians:
         print("The magician is " + magician + "!")
 
 show_magicians(magicians)
+"""
