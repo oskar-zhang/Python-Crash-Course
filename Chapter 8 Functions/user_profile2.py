60089a5d1ca64b7a30d2b6ebc13efab46ee3430d
oskarsforest <oskarzhang1203@gmail.com>
2019-08-05 21:43:55 -0700

    Try It Yourself

--- Chapter 8 Functions/user_profile2.py
+++ Chapter 8 Functions/user_profile2.py
@@ -4,6 +4,44 @@ a profile of yourself by calling build_profile(), using your first and last name
 and three other key-value pairs that describe you.
 """
 
+def build_profile (firstname, lastname, **description):
+    myself = {}
+    myself['First Name'] = firstname
+    myself['Last Name'] = lastname
+
+    for key, value in description.items():
+        myself[key.title()] = value.title()
+    
+    return myself
+
+my_profile = build_profile('Oskar', 'Zhang', Glass = 'Yes', Race = 'Asian', 
+                            sports = 'soccer')
+
+print(my_profile)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 def build_profile(first, last, **characteristics):
     profile = {}
     profile['First Name'] = first.title()
@@ -16,3 +54,4 @@ myself = build_profile('oskar', 'zhang', Race = 'asian',
                 Job = 'developer', Nationality = 'china')
 
 print(myself)
+"""
