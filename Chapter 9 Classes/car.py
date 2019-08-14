6d0bbbeed935da618884b1b5213b16042d83fc3a
oskarsforest <oskarzhang1203@gmail.com>
2019-08-13 23:17:02 -0700

    Modifying an Attribute’s Value Directly

--- Chapter 9 Classes/car.py
+++ Chapter 9 Classes/car.py
@@ -19,3 +19,6 @@ class Car():
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
+
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
